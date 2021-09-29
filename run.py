f5d8bf72a953f90f74a8f7de572a047bfe412437
neso613 <31642462+neso613@users.noreply.github.com>
2021-09-29 14:53:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,26 +5,28 @@ import traceback
 import numpy as np
 import cv2
 import time
-import onnxruntime as ort
+import onnxruntime as rt
 
 CLASSES = open('action_recognition_kinetics_moments.txt').read().strip().split("\n")
 DURATION = 16
 INPUT_SIZE = 112
-stream = 'act.gif'
+stream = '-0HKFF7F_BY_000003_000013.mp4'
 model = 'resnet-18-kinetcis-moments.onnx'
 frameskip = 2
-save_output = ''
+save_output = 'filename.mp4'
 
 def onnx_load_model(onnx_model):
     sess = rt.InferenceSession(onnx_model,None)
     return sess
 
-def run_inference(host_input):
+def run_inference(host_input,sess):
     input_name = sess.get_inputs()[0].name
     output_name = sess.get_outputs()[0].name
     outputs = ''
     outputs = sess.run([output_name], {input_name: host_input})
-    return CLASSES[np.argmax(outputs)]
+    preds = CLASSES[np.argmax(outputs)]
+    print(preds)
+    return preds
 
 if __name__ == '__main__':
     if stream == '':
@@ -35,7 +37,7 @@ if __name__ == '__main__':
         print('[Error] Please provide a valid path --model.')
         sys.exit(0)
 
-    model = onnx_load_model(model)
+    sess = onnx_load_model(model)
 
     if not save_output == '':
         writer = cv2.VideoWriter(save_output,cv2.VideoWriter_fourcc(*'MJPG'),60, (1920, 1080))
@@ -59,16 +61,15 @@ if __name__ == '__main__':
                 blob = np.expand_dims(blob, axis=0)
                 blob = np.ascontiguousarray(blob)
                 start = time.time()
-                result = run_inference(blob)
-                inferencetime = round(time.time() - start, 4)
-                print('Inference Time: {} ms'.format(inferencetime), end='\r')
+                result = run_inference(blob,sess)
+                inferencetime = time.time() - start
         overlay = frame.copy()
         display = frame.copy()
         cv2.rectangle(overlay, (560, 850), (1360, 1000), (0, 0, 0), -1)
         cv2.putText(overlay, 'Inference Time: {} s'.format(inferencetime), (600, 900), cv2.FONT_HERSHEY_COMPLEX,
-            1.25, (255, 255, 255), 2)
+            1.25, (255, 250, 50), 2)
         cv2.putText(overlay, 'Output: {}'.format(result), (600, 950), cv2.FONT_HERSHEY_COMPLEX,
-            1.25, (0, 255, 0), 2)
+            1.25, (120, 255, 100), 2)
         cv2.addWeighted(overlay, 0.7, display, 1 - 0.7, 0, display)
         cv2.imshow('Output', display)
         if not save_output == '':
